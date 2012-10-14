5873831c59c8b531b785650c49ca4a3a5e8644a3
rekha1986 <rekha.benada@gmail.com>
2012-10-14 15:48:19 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RealApp1::Application.config.secret_token = '628a5b8c6c7f1fb968cb20efb41203beb6397b929016294215d3edd74c48004d669256fa66b373d51a4767201029ecf5db46acd6a487707f38cdadcd5ed29839'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+RealApp1::Application.config.session_store :cookie_store, key: '_real_app1_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# RealApp1::Application.config.session_store :active_record_store
