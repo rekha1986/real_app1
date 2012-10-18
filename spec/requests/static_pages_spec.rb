35b0d9f5dd12cd43e228174e8094030cedf19240
rekha1986 <rekha.benada@gmail.com>
2012-10-18 11:00:12 -0700

    add a full titile helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,11 +9,20 @@ describe "Static Pages" do
 
     end
 
-it "should have the title 'Home'" do
+it "should have the base title" do
       visit '/static_pages/home'
       page.should have_selector('title',
-                        :text => "My Real App1 | Home")
+                        :text => "My Real App1")
     end
+
+it "should not have a custom page title" do
+visit '/static_pages/home'
+page.should_not have_selector('title', :text => '|Home')
+end
+
+
+
+
 end
 
 describe "Help page" do
@@ -23,11 +32,19 @@ describe "Help page" do
       page.should have_selector('h1', :text => 'Help')
 end
 
-it "should have the title 'Help'" do
+it "should have the base title" do
       visit '/static_pages/help'
       page.should have_selector('title',
-                        :text => "My Real App1 | Help")
+                        :text => "My Real App1")
     end
+
+it "should not have a custom page title" do
+      visit '/static_pages/help'
+      page.should_not have_selector('title', :text => '| Help')
+    end
+
+
+
 end
 
 describe "About page" do
@@ -37,10 +54,15 @@ visit '/static_pages/about'
 page.should have_selector('h1', :text => 'About Us')
 end
 
-it "should have the title 'About Us'" do
+it "should have the base title" do
       visit '/static_pages/about'
       page.should have_selector('title',
-                        :text => "My Real App1 | About Us")
+                        :text => "My Real App1")
+    end
+
+it "should not have a custom page title" do
+      visit '/static_pages/about'
+      page.should_not have_selector('title', :text => '| About Us')
     end
 end
 end
