12f96394fc67ee2ae3a83577f3ef35f9cf46c295
rekha1986 <rekha.benada@gmail.com>
2012-11-01 12:45:55 -0700

    finally done with signin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,6 @@
 class SessionsController < ApplicationController
 
 
-<<<<<<< HEAD
  def new
   end
 
@@ -9,7 +8,6 @@ def create
 
 user = User.find_by_email(params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # Sign the user in and redirect to the user's show page.
 sign_in user
 redirect_to user
     else
@@ -23,24 +21,4 @@ sign_out
 redirect_to root_url
 end
 
-=======
-def new
-end
-
-def create
-user = User.find_by_email(params[:session][:email].downcase)
-if user && user.authenticate(params[:session][:password])
-sign_in user
-redirect_to  user
-else
-flash.now[:error] = 'invalid email/password combination' 
-render 'new'
-end
-end
-
-def destroy
-end
-
-
->>>>>>> sign-up
 end
