4000696ca23978fc7ddbc9d82d9db806f7207230
rekha1986 <rekha.benada@gmail.com>
2012-11-02 00:34:12 -0700

    finish user edit, update,index and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -21,7 +21,9 @@ def current_user
   end
 
 
-
+def current_user?(user)
+user == current_user
+end
 
 
 def sign_out
@@ -29,4 +31,17 @@ self.current_user = nil
 cookies.delete(:remember_token)
 
 end
+
+def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    session.delete(:return_to)
+  end
+
+  def store_location
+    session[:return_to] = request.url
+  end
+
+
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -31,9 +31,6 @@ private
 
 
 
-def create_remember_token
-self.remember_token = SecureRandom.urlsafe_base64
-end
 
 
 end
